a7f2b792412446729144965e495028c530b1cae1
David Benner <davidbenner@Davids-MacBook-Pro.local>
2023-01-20 21:38:28 -0500

    complete kitten, rating, review schema

---
+++ models/kittens.js
@@ -0,0 +1,78 @@
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: false,
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["Male", "Female", "Unknown"],
+  },
+  breed: {
+    type: String,
+    enum: [
+      "Persian",
+      "Maine Coon",
+      "Ragdoll",
+      "Siamese",
+      "British Shorthair",
+      "Exotic Shorthair",
+      "Abyssinian",
+      "Tonkinese",
+      "Russian Blue",
+      "Bengal",
+      "Scottish Fold",
+      "Siberian",
+      "Birman",
+      "Himalayan",
+      "Nebelung",
+      "Norwegian Forest",
+      "Bombay",
+      "Burmese",
+      "Chartreux",
+      "American Shorthair",
+      "Mutt",
+      "Other",
+    ],
+  },
+  bio: {
+    type: String,
+  },
+  ratings: [ratingSchema],
+
+  review: [reviewSchema],
+});
+
+const reviewSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    userName: String,
+    userAvatar: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ratingSchema = new Schema({
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 5,
+  },
+});
