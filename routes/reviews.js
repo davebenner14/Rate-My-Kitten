0ad7bc29bbbd1a844b09b2ef40916db9da57a822
David Benner <davidbenner@Davids-MBP.ht.home>
2023-01-23 12:27:15 -0500

    added reviews.js to routes

---
+++ routes/reviews.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const reviewsCtrl = require("../controllers/reviews");
+const ensureLoggedIn = require("../config/ensureLoggedIn");
+
+router.post("/kittens/:id/reviews", ensureLoggedIn, reviewsCtrl.create);
+router.delete("/reviews/:id", ensureLoggedIn, reviewsCtrl.delete);
+
+module.exports = router;
