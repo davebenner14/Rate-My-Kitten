bb362090e47fc65ac0f6d75129409f22b45db95f
David Benner <davidbenner@Davids-MBP.ht.home>
2023-01-23 12:23:28 -0500

    added reviews.js in controllers

---
+++ controllers/reviews.js
@@ -0,0 +1,36 @@
+const Kitten = require("../models/kitten");
+
+module.exports = {
+  create,
+  delete: deleteReview,
+};
+
+function deleteReview(req, res, next) {
+  Kitten.findOne({
+    "reviews._id": req.params.id,
+    "reviews.user": req.user._id,
+  }).then(function (kitten) {
+    if (!kitten) return res.redirect("/kittens");
+    kitten.reviews.remove(req.params.id);
+    kitten
+      .save()
+      .then(function () {
+        res.redirect(`/kittens/${kitten._id}`);
+      })
+      .catch(function (err) {
+        return next(err);
+      });
+  });
+}
+
+function create(req, res) {
+  Kitten.findById(req.params.id, function (err, kitten) {
+    req.body.user = req.user._id;
+    req.body.userName = req.user.name;
+    req.body.userAvatar = req.user.avatar;
+    kitten.reviews.push(req.body);
+    kitten.save(function (err) {
+      res.redirect(`/kittens/${kitten._id}`);
+    });
+  });
+}
