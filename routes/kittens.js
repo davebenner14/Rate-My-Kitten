a6001a243bd9a40f4ac3db2d5b8cc7d44569bc22
David Benner <davidbenner@Davids-MacBook-Pro.local>
2023-01-24 12:34:15 -0500

    continuing add photo/debugging

--- routes/kittens.js
+++ routes/kittens.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 const kittensCtrl = require("../controllers/kittens");
 const ensureloggedIn = require("../config/ensureLoggedIn");
 // const photo = require("../controllers/photos");
 
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: path.join(__dirname, "..", "uploads") });
 
 router.get("/", kittensCtrl.index);
 router.get("/new", ensureloggedIn, kittensCtrl.new);
